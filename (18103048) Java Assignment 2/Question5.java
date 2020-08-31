0f0803c4d8c6014136b686c8e9bf593d4fecab2b
saksham117 <55136108+saksham117@users.noreply.github.com>
2020-09-01 00:23:46 +0530

    Update Question5.java

--- (18103048) Java Assignment 2/Question5.java
+++ (18103048) Java Assignment 2/Question5.java
@@ -1,5 +1,6 @@
 import java.util.*;
 import java.io.*;
+import java.time.*;
 
 public class Question5{
 
@@ -53,7 +54,7 @@ public class Question5{
         System.out.println();
 
 
-
+        Instant start1 = Instant.now();
         // performing operation using sets as arrays
 
         // union
@@ -109,9 +110,49 @@ public class Question5{
         System.out.println("The compliment of set B is " + complement_a_B);
 
 
+        System.out.println();
 
+        Instant end1 = Instant.now();
+
+        Duration timeElapsed1 = Duration.between(start1, end1);
 
         // performing operation using Sets class
+
+        Instant start2 = Instant.now();
+        //union
+        Set<Integer> union = new HashSet<Integer>(setC);  
+        union.addAll(setD);  
+  
+        //intersection  
+        Set<Integer> intersection = new HashSet<Integer>(setC);  
+        intersection.retainAll(setD);  
+
+        //complement
+        Set<Integer> complement_C = new HashSet<Integer>();  
+        complement_C.addAll(Arrays.asList(new Integer[] {0,1,2,3,4,5,6,7,8,9,10})); 
+
+
+        Set<Integer> complement_D = new HashSet<Integer>();  
+        complement_D.addAll(Arrays.asList(new Integer[] {0,1,2,3,4,5,6,7,8,9,10}));
+
+
+        complement_C.removeAll(setC);
+        complement_D.removeAll(setD); 
+
+
+        System.out.println("The union of the two sets is " + union);
+        System.out.println("The intersection of the two sets is " + intersection);
+        System.out.println("The compliment of set A is " + complement_C);
+        System.out.println("The compliment of set B is " + complement_D);
+        System.out.println();
+
+        Instant end2 = Instant.now();
+
+        Duration timeElapsed2 = Duration.between(start2, end2);
+
+
+        System.out.println("The time taken in ths first approach is:" + timeElapsed1);
+        System.out.println("The time taken in ths second approach is:" + timeElapsed2);
 
 	}
 }
